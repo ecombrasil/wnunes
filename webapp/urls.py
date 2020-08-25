a3a2d46cac467aa60c1045fc0b4a873c6adae1f1
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2020-08-25 17:43:11 -0300

    Create register view

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,11 +3,13 @@ from django.views.generic import TemplateView
 from .views import (
     Inicio,
     Entrar,
+    CriarConta,
     Sair,
 )
 
 urlpatterns = [
     path('', Inicio.as_view(), name='inicio'),
     path('entrar', Entrar.as_view(), name='entrar'),
+    path('criar-conta', CriarConta.as_view(), name='criar_conta'),
     path('sair', Sair.as_view(), name='sair')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login, logout
+from .models import User
 from lets_debug import DecoratorTools as debug
 
 
@@ -19,12 +21,26 @@ class Entrar(View):
         else:
             return render(request, 'entrar.html', { 'error': True })
 
+class CriarConta(View):
+    def get(self, request):
+        return render(request, 'criar.conta.html', { 'error': False })
+
+    def post(self, request):
+        email = request.POST['email']
+        cpf = request.POST['cpf']
+        senha = request.POST['senha']
+
+        if email is not None and cpf is not None and senha is not None:
+            usuario = User.objects.create(email=email, cpf=cpf, password=senha)
+            login(request, usuario)
+            return redirect('inicio')
+        return render(request, 'criar.conta.html', { 'error': True })
+
 class Sair(View):
     def get(self, request):
         if request.user.is_authenticated:
             logout(request)
         return redirect('entrar')
 
-class Inicio(View):
-    def get(self, request):
-        return render(request, 'inicio.html')
+class Inicio(TemplateView):
+    template_name = 'inicio.html'
