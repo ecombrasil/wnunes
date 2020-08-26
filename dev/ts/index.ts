d3cba5e8e1f919dad0a5eb1f209131a650900109
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2020-08-26 14:24:16 -0300

    Add shadow to header when scrolling

--- dev/ts/index.ts
+++ dev/ts/index.ts
@@ -1,7 +1,19 @@
 import { handleBindingAttr } from './utils';
 
+// Avoid images from getting arrested by the user
 document.body.ondragstart = () => false;
 
+window.addEventListener('load', () => {
+  const headerElement = document.querySelector('#header');
+  const mainElement = document.querySelector('main');
+
+  // Add shadows to the header after scrolling more than 70 pixels
+  mainElement.addEventListener('scroll', () =>
+    headerElement.classList.toggle('shaded-header', mainElement.scrollTop > 70
+  ));
+});
+
+// Enable elements with the atribute clickAndGo to be clicked to open links
 handleBindingAttr('clickAndGo', (element, value) =>
   element.addEventListener('click', () => window.location.href = value)
 );
