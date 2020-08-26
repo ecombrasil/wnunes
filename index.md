0ef32d278e6599face31faf292256902983b655b
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2020-08-26 17:38:34 -0300

    Keep working on homepage

--- index.md
+++ index.md
@@ -1,12 +1,5 @@
 # Atividades concluídas
 
-## Ter, 25 de agosto
-- Página de login
-- Página de cadastro
-
-![Screenshot da tela Entrar](https://i.imgur.com/UM9kbPH.png)
-![Screenshot da tela Criar conta](https://i.imgur.com/IzOSalH.png)
-
 ## Qua, 26 de agosto
 - Autenticação de usuários
 - Área administrativa
@@ -14,3 +7,10 @@
 ![Screenshot da tela Entrar na administração](https://i.imgur.com/HfibP76.png)
 ![Screenshot da tela Admininstração](https://i.imgur.com/GWlrfWs.png)
 ![Screenshot da tela Admininstração Usuários](https://i.imgur.com/YwBIfZ7.png)
+
+## Ter, 25 de agosto
+- Página de login
+- Página de cadastro
+
+![Screenshot da tela Entrar](https://i.imgur.com/UM9kbPH.png)
+![Screenshot da tela Criar conta](https://i.imgur.com/IzOSalH.png)
--- wnunes/static/js/main.js
+++ wnunes/static/js/main.js
@@ -4,12 +4,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("./utils");
 // Avoid images from getting arrested by the user
 document.body.ondragstart = () => false;
-window.addEventListener('load', () => {
-    const headerElement = document.querySelector('#header');
-    const mainElement = document.querySelector('main');
-    // Add shadows to the header after scrolling more than 70 pixels
-    mainElement.addEventListener('scroll', () => headerElement.classList.toggle('shaded-header', mainElement.scrollTop > 70));
-});
 // Enable elements with the atribute clickAndGo to be clicked to open links
 utils_1.handleBindingAttr('clickAndGo', (element, value) => element.addEventListener('click', () => window.location.href = value));
 
