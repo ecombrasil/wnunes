b11735e5e64ac788df10b384cf2fac860478f0ca
luizfilipezs <filipeluiz.bs@gmail.com>
2021-01-11 20:10:23 -0300

    Corrigir inicialização de componente no decorator Component

--- dev/ts/Component.ts
+++ dev/ts/Component.ts
@@ -1,12 +1,12 @@
 import { Type } from 'easy-coding/lib/types';
 
 /**
- * Initializes a class binding its constructor to an element in the DOM.
+ * Initializes a class, binding its constructor to an element in the DOM.
  * 
  * @param {string} name Component's query selector.
  */
 const Component = (name: string) => <T extends Type>(type: T): T => {
-  [...document.querySelectorAll(name)].forEach((element) => type.bind(element));
+  [...document.querySelectorAll(name)].forEach((element) => new (type.bind(null, element)));
 
   return type;
 }
